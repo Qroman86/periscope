557acaf801bb1b127900da153635b55134b4524b
Qroman86 <roman.kudr@gmail.com>
2018-09-14 16:11:37 +0300

    Симулятор игры "Augustus". Каркас мастера игры

--- augustus.py
+++ augustus.py
@@ -2,12 +2,43 @@ import json
 
 
 class GameMaster:
+
     def __init__(self):
         self.title = 'GameMaster'
 
-    def preparePack(self):
-        self.pack = Pack()
-        return self.pack
+    def prepareBoard(self):
+
+        data = {}
+        with open('cards.txt') as json_file:
+            data = json.load(json_file)
+
+        self.board = Board()
+        self.board.preparePack()
+
+    def startGame(self):
+        round = 1
+        z = 'continue'
+        while round > 0:
+            z = raw_input('Round ' + str(round) + ' ')
+            if z == "stop":
+                break
+            if z == "next round":
+                round = round + 1
+
+    def newGame(self):
+        print('new game')
+        print('Input number of gamers (2-4)')
+        self.number_of_gamers = input()
+        # ToDo write wrapper for exceptions
+
+        self.prepareBoard()
+        self.startGame()
+
+    def loadGame(self):
+        print('load game')
+
+    def saveGame(self):
+        print('save game')
 
 
 class Pack:
@@ -19,9 +50,12 @@ class Board:
     def __init__(self):
         self.title = 'Board'
 
+    def preparePack(self):
+        self.pack = Pack()
+        return self.pack
 
 class Gamer:
-    def __init__(self):
+    def __init__(self, number, name):
         self.title = 'Gamer'
 
 
@@ -35,9 +69,11 @@ class Token:
         self.title = 'Token'
 
 
-gameMaster = GameMaster()
-pack = gameMaster.preparePack()
-print(pack.title)
+count = 1
+for card in data['cards']:
+    # print('Name: ' + card['name'] + ' count=' + str(count))
+    count = count + 1
 
-with open('cards.txt') as json_file:
-    data = json.load(json_file)
+
+gameMaster = GameMaster()
+gameMaster.newGame()
