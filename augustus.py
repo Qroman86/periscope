c02b9e6d83f7b0dedfa0144df6b7138e3f82031e
Qroman86 <roman.kudr@gmail.com>
2018-09-17 16:58:54 +0300

    Симулятор игры "Augustus". Каркас мастера игры: подготовка к игре

--- augustus.py
+++ augustus.py
@@ -1,35 +1,130 @@
 import json
 
 
+class Action:
+    def __init__(self):
+        self.title = 'Action'
+
+    def printOut(self):
+        printResult = '\nAction number:' + str(self.number)
+        printResult += '\ntype:' + self.type
+        printResult += '\nwhat:' + self.what
+        printResult += '\nname:' + self.name
+        printResult += '\nplayerid:' + str(self.playerid)
+        print(printResult)
+
+
+class Round:
+    def __init__(self):
+        self.actions = []
+        self.title = 'Round'
+
+    def getNumber(self):
+        return self.number
+
+    # prepartion's actions
+    def addGamerAction(self, player):
+        addGamerAction = Action()
+        addGamerAction.type = 'add'
+        addGamerAction.what = 'player'
+        addGamerAction.name = player.name
+        addGamerAction.playerid = player.id
+        addGamerAction.number = len(self.actions) + 1
+        self.actions.append(addGamerAction)
+
+    def printOut(self):
+        print('Round number ' + str(self.number))
+        for action in self.actions:
+            action.printOut()
+
+
+class LogRecord:
+    def __init__(self, logRecords):
+        self.title = 'logRecord'
+        self.id = len(logRecords) + 1
+        logRecords.append(self)
+
+    def printOut(self):
+        print('LogRecord id:' + str(self.id))
+        if (self.type == 'add' and self.what == 'player'):
+            print('Add player id:' + str(self.playerid) + ' name:' + str(self.playername))
+
+
+class Logger:
+
+    def __init__(self):
+        self.logRecords = []
+
+    def logAddPlayer(self, player):
+        logAddPlayer = LogRecord(self.logRecords)
+        logAddPlayer.type = 'add'
+        logAddPlayer.what = 'player'
+        logAddPlayer.playername = player.name
+        logAddPlayer.playerid = player.id
+
+    def printAllRecords(self):
+        print('\nStart print all log records')
+        for logRecord in self.logRecords:
+            logRecord.printOut()
+        print('End print all log records\n')
+
+
+class LaterRemoveMe:
+    def __init__(self):
+        self.rounds = []
+        self.title = 'Logger'
+
+    def addPrepareRound(self):
+        self.rounds = []
+        round = Round()
+        round.number = 0
+        self.rounds.append(round)
+
+    def addNewRound(self):
+        round = Round()
+        round.number = len(self.rounds) + 1
+        self.rounds.append(round)
+
+    def getCurrentRound(self):
+        return self.rounds[len(self.rounds) - 1]
+
+    def addGamerAction(self, player):
+        self.getCurrentRound().addGamerAction(player)
+
+    def printOutCurrentRound(self):
+        self.getCurrentRound().printOut()
+
 class GameMaster:
 
     def __init__(self):
         self.title = 'GameMaster'
+        self.logger = Logger()
 
     def prepareBoard(self):
 
+
         self.board = Board()
+        self.board.logger = self.logger
         self.board.preparePack()
         self.board.initGamers(self.number_of_gamers)
 
 
 
     def startGame(self):
-        round = 1
+        self.round = 1
         z = 'continue'
-        while round > 0:
-            z = raw_input('Round ' + str(round) + ' ')
+        while self.round > 0:
+            z = raw_input('Round ' + str(self.round) + ' ')
             if z == "stop":
                 break
             if z == "next round":
-                round = round + 1
+                self.round = self.round + 1
 
     def newGame(self):
         print('new game')
         print('Input number of gamers (2-4)')
         self.number_of_gamers = input()
         # ToDo write wrapper for exceptions
-
         self.prepareBoard()
         self.startGame()
 
@@ -81,13 +176,13 @@ class Board:
         x = range(1, number_of_gamers + 1)
         for n in x:
             gamer = Gamer()
+            gamer.logger = self.logger
             gamer.id = n
             gamer.name = 'Gamer ' + str(gamer.id)
             self.gamers.append(gamer)
             self.setFirstCard(gamer)
-            print(gamer.name)
-
-
+            self.logger.logAddPlayer(gamer)
+        self.logger.printAllRecords()
 
 class Gamer:
     def __init__(self):
@@ -109,7 +204,8 @@ class Card:
         self.gold = cardItem.get('gold')
         self.wheat = cardItem.get('wheat')
         self.action = cardItem.get('action')
-
+        self.bonus = cardItem.get('bonus')
+        self.feature = cardItem.get('feature')
         # "tokens":["sw", "sw", "st", "st", "da"],
         #"action":{"type": "remove", "what": "legions", "times": "all_from_one_card"}
 
